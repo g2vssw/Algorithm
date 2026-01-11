d2f781706fec28cb882e3bf3cc6988bbc82dbfd7
g2vssw <g2vssw@gmail.com>
2026-01-11 22:52:24 +0900

    [level 0] Title: 합성수 찾기, Time: 0.06 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = 0
+    if n > 3:
+        for num in range(4, n + 1):
+            for i in range(2, num):
+                if num % i == 0:
+                    answer += 1
+                    break
+    return answer
