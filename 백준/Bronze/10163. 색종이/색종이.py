97187d4ce98fa124491d77e70ee61d9b079d1ae7
g2vssw <g2vssw@gmail.com>
2026-01-26 17:48:26 +0900

    [Bronze I] Title: 색종이, Time: 420 ms, Memory: 116720 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,16 +1,18 @@
 N = int(input())
-num_list = [list(map(int, input().split())) for _ in range(N)]
+arr= [[0] * 1001 for _ in range(1001)]
 
-arr = [[0 for _ in range(1001)] for _ in range(1001)]
+for num in range(1, N + 1):
+    si, sj, w, h = map(int, input().split())
+    ei, ej = si + w, sj + h
+    for i in range(si, ei):
+        for j in range(sj, ej):
+            arr[i][j] = num
+    
+answer = [0] * (N + 1)
+for i in range(1001):
+    for j in range(1001):
+        if arr[i][j] != 0:
+            answer[arr[i][j]] += 1
 
-for num in range(N):
-    for i in range(num_list[num][0], num_list[num][0] + num_list[num][2]):
-        for j in range(num_list[num][1], num_list[num][1] + num_list[num][3]):
-            arr[i][j] = num + 1
-
-for i in range(1, N + 1):
-    num = 0
-    for li in arr:
-        num += li.count(i)
-    print(num)
-    num = 0
+for num in range(1, N + 1):
+    print(answer[num])
