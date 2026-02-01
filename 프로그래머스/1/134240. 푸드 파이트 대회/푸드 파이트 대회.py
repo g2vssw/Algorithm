264fc078df99cde754c6a1e70b0db54922f362c6
g2vssw <g2vssw@gmail.com>
2026-02-02 01:34:55 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,7 @@
+def solution(food):
+    answer = '0'
+    for i in range(len(food) - 1, 0, -1):
+        n = food[i] // 2
+        if n >= 1:
+            answer = str(i) * n + answer + str(i) * n
+    return answer
