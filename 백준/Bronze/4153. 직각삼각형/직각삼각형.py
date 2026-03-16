6d1645f919decf132c5f9c5f115b585c58797ced
g2vssw <g2vssw@gmail.com>
2026-03-16 21:18:08 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    
+    arr = list(map(int, input().split()))
+
+    if sum(arr) == 0:
+        break
+
+    arr.sort()
+
+    if arr[2] ** 2 == arr[0] ** 2 + arr[1] ** 2:
+        print("right")
+    else:
+        print("wrong")
