ba5921bdcd4f566d03c16a7b10535638235cd54e
g2vssw <g2vssw@gmail.com>
2026-03-22 22:25:01 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+arr = list(map(int, input().split()))
+
+if arr == sorted(arr):
+    print("ascending")
+elif arr == sorted(arr, reverse=True):
+    print("descending")
+else:
+    print("mixed")
