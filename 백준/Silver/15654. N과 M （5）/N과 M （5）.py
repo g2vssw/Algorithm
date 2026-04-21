1c2c0bc2576937612ee1a7b098f1db6248fa3339
g2vssw <g2vssw@gmail.com>
2026-04-21 19:16:26 +0900

    [Silver III] Title: N과 M (5), Time: 164 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -3,20 +3,21 @@ def recur():
         print(*path)
         return
 
-    for i in range(N):
+    for i in li:
         if used[i]:
             continue
 
         used[i] = True
-        path.append(li[i])
+        path.append(i)
         recur()
         path.pop()
         used[i] = False
+
 
 N, M = map(int, input().split())
 li = list(map(int, input().split()))
 li.sort()
 
 path = []
-used = [False] * N
+used = [False] * (li[-1] + 1)
 recur()
