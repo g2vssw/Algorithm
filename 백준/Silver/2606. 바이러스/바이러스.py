1d1ffa3276dc0b17da37d50540f422607e3b3247
g2vssw <g2vssw@gmail.com>
2026-04-22 18:54:01 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,30 @@
+def bfs(node):
+    queue = [node]
+
+    while queue:
+        node = queue.pop()
+
+        for next_node in graph[node]:
+            if visited[next_node] == 1:
+                continue
+            visited[next_node] = 1
+            queue.append(next_node)
+
+N = int(input())
+M = int(input())
+graph = [[] for _ in range(N + 1)]
+visited = [0] * (N + 1)
+
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1, N + 1):
+    graph[i].sort()
+
+visited[1] = 1
+bfs(1)
+
+print(visited.count(1) - 1)
