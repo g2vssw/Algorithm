9a4ab6ef57467d184c187499e318e5a7658b79f3
g2vssw <g2vssw@gmail.com>
2026-04-23 20:49:14 +0900

    [Silver I] Title: 미로 탐색, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,24 +1,12 @@
-N, M = map(int, input().split())
-arr = [list(map(int, input())) for _ in range(N)]
-di = [-1, 0, 1, 0]
-dj = [0, 1, 0, -1]
-queue = [(0, 0)]
-
-while queue:
-    i, j = queue.pop(0)
-
-    if i == N - 1 and j == M - 1:
-        break
-
-    for k in range(4):
-        ni, nj = i + di[k], j + dj[k]
-
-        if ni < 0 or ni >= N or nj < 0 or nj >= M:
-            continue
-        elif arr[ni][nj] == 0:
-            continue
-        elif arr[ni][nj] == 1:
-            arr[ni][nj] = arr[i][j] + 1
-            queue.append((ni, nj))
-
-print(arr[N-1][M-1])
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
