994776adbd6aecac2815483ef0a14b07a06f6dae
g2vssw <g2vssw@gmail.com>
2026-04-23 20:36:48 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -8,16 +8,17 @@ while queue:
     i, j = queue.pop(0)
 
     if i == N - 1 and j == M - 1:
-        print(arr[i][j])
         break
 
     for k in range(4):
-        ni = i + di[k]
-        nj = j + dj[k]
+        ni, nj = i + di[k], j + dj[k]
+
         if ni < 0 or ni >= N or nj < 0 or nj >= M:
             continue
-        elif arr[ni][nj] != 1:
+        elif arr[ni][nj] == 0:
             continue
         elif arr[ni][nj] == 1:
             arr[ni][nj] = arr[i][j] + 1
             queue.append((ni, nj))
+
+print(arr[N-1][M-1])
