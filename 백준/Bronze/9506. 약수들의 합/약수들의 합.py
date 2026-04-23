1405b6060c3463a68ae3f722682fcc9f6e4048de
g2vssw <g2vssw@gmail.com>
2026-04-23 20:47:45 +0900

    [Bronze I] Title: 약수들의 합, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -1,29 +1,12 @@
-import sys
-sys.setrecursionlimit(10**6)
-
-def divisor(n):
-    global K
-    if n == 1:
-        return
-    if N % n == 0:
-        K.add(N // n)
-    divisor(n-1)
-
-
-while True:
-    N = int(input())
-    K = set()
-
-    if N == -1:
-        break
-
-    divisor(N)
-    
-    li = list(K)
-    li.sort()
-
-    if sum(K) == N:
-        result = ' + '.join(map(str, li))
-        print(f'{N} = {result}')
-    else:
-        print(f'{N} is NOT perfect.')
+<html>
+<head><title>502 Bad Gateway</title></head>
+<body>
+<center><h1>502 Bad Gateway</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
