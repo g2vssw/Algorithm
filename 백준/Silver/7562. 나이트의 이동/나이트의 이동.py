707abde7f57ef0d0849ad6546997a50b997531ff
g2vssw <g2vssw@gmail.com>
2026-04-23 20:36:20 +0900

    [Silver I] Title: 나이트의 이동, Time: 3228 ms, Memory: 33464 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,27 +1,27 @@
-N = int(input())
-di = [-2, -1, 1, 2, 2, 1, -1, -2]
-dj = [1, 2, 2, 1, -1, -2, -2, -1]
+T = int(input())
 
-for _ in range(N):
-    I = int(input())
-    arr = [[0 for _ in range(I)] for _ in range(I)]
-    si, sj = map(int, input().split())
-    ei, ej = map(int, input().split())
-    queue = [(si, sj)]
+for _ in range(T):
+    N = int(input())
+    queue = [tuple((map(int, input().split())))]
+    e_i, e_j = map(int, input().split())
+    arr = [[0 for _ in range(N)] for _ in range(N)]
+    visited = [[0 for _ in range(N)] for _ in range(N)]
+    di = [-2, -1, 1, 2, 2, 1, -1, -2]
+    dj = [1, 2, 2, 1, -1, -2, -2, -1]
 
     while queue:
         i, j = queue.pop(0)
 
-        if i == ei and j == ej:
-            print(arr[i][j])
+        if i == e_i and j == e_j:
             break
 
         for k in range(8):
             ni, nj = i + di[k], j + dj[k]
-            if ni < 0 or ni >= I or nj < 0 or nj >= I:
-                continue
-            elif arr[ni][nj] != 0:
+
+            if ni < 0 or ni >= N or nj < 0 or nj >= N:
                 continue
             elif arr[ni][nj] == 0:
-                queue.append((ni, nj))
                 arr[ni][nj] = arr[i][j] + 1
+                queue.append((ni, nj))
+
+    print(arr[e_i][e_j])
