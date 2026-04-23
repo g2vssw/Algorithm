d75e032ef80117eec30ef29834535ab9f5fdadc7
g2vssw <g2vssw@gmail.com>
2026-04-23 20:47:48 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 388 ms, Memory: 32472 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -1,20 +1,12 @@
-def recursion(s, l, r):
-    global cnt
-    cnt += 1
-    if l >= r:
-        return 1
-    elif s[l] != s[r]:
-        return 0
-    else:
-        return recursion(s, l+1, r-1)
-
-def isPalindrome(s):
-    return recursion(s, 0, len(s)-1)
-
-N = int(input())
-
-for _ in range(N):
-    S = input()
-    cnt = 0
-
-    print(isPalindrome(S), cnt)
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
