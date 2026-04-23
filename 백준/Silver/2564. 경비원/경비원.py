dc4648b18f58a4f928f4a006ca0d7b66acc2dcf8
g2vssw <g2vssw@gmail.com>
2026-04-23 20:13:34 +0900

    [Silver I] Title: 경비원, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2564. 경비원/경비원.py
+++ 백준/Silver/2564. 경비원/경비원.py
@@ -6,43 +6,43 @@ start = tuple(map(int, input().split()))
 total = 0
 for i in range(N):
     if start[0] == 1:
-        if end[i][0] == 1:
+        if end[i][0] == 1:  
             total += abs(start[1] - end[i][1])
-        elif end[i][0] == 2:
-            total += min(start[1] + end[i][1], w * 2 - end[i][1] - start[1]) + l
-        elif end[i][0] == 3:
+        elif end[i][0] == 2: 
+            total += min(start[1] + end[i][1] + l, 2 * w - (start[1] + end[i][1]) + l)
+        elif end[i][0] == 3: 
             total += end[i][1] + start[1]
-        elif end[i][0] == 4:
-            total += end[i][1] + w - start[1]
+        elif end[i][0] == 4:  
+            total += end[i][1] + (w - start[1])
 
     elif start[0] == 2:
-        if end[i][0] == 1:
-            total += min(start[1] + end[i][1], w * 2 - end[i][1] - start[1]) + l
-        elif end[i][0] == 2:
+        if end[i][0] == 2: 
             total += abs(start[1] - end[i][1])
-        elif end[i][0] == 3:
-            total += l - end[i][1] + start[1]
-        elif end[i][0] == 4:
-            total += l - end[i][1] + w - start[1]
+        elif end[i][0] == 1:
+            total += min(start[1] + end[i][1] + l, 2 * w - (start[1] + end[i][1]) + l)
+        elif end[i][0] == 3: 
+            total += (l - end[i][1]) + start[1]
+        elif end[i][0] == 4:  
+            total += (l - end[i][1]) + (w - start[1])
 
     elif start[0] == 3:
-        if end[i][0] == 1:
-            total += end[i][1] + start[1]
-        elif end[i][0] == 2:
-            total += l - start[1] + end[i][1]
-        elif end[i][0] == 3:
+        if end[i][0] == 3:  
             total += abs(start[1] - end[i][1])
-        elif end[i][0] == 4:
-            total += min(start[1] + end[i][1], l * 2 - end[i][1] - start[1]) + w
+        elif end[i][0] == 4: 
+            total += min(start[1] + end[i][1] + w, 2 * l - (start[1] + end[i][1]) + w)
+        elif end[i][0] == 1:  
+            total += end[i][1] + start[1]
+        elif end[i][0] == 2:  
+            total += (l - start[1]) + end[i][1]
 
     elif start[0] == 4:
-        if end[i][0] == 1:
-            total += w - end[i][1] + start[1]
-        elif end[i][0] == 2:
-            total += w - end[i][1] + start[1]
-        elif end[i][0] == 3:
-            total += min(start[1] + end[i][1], l * 2 - end[i][1] - start[1]) + w
-        elif end[i][0] == 4:
+        if end[i][0] == 4: 
             total += abs(start[1] - end[i][1])
+        elif end[i][0] == 3:  
+            total += min(start[1] + end[i][1] + w, 2 * l - (start[1] + end[i][1]) + w)
+        elif end[i][0] == 1:  
+            total += (w - end[i][1]) + start[1]
+        elif end[i][0] == 2:  
+            total += (w - end[i][1]) + start[1]
 
 print(total)
