628963d7199366da1419ebf9548ac1c2940c5a86
g2vssw <g2vssw@gmail.com>
2026-04-23 20:37:42 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,36 +1,12 @@
-N = int(input())
-switch = list(map(int, input().split()))
-stu_num = int(input())
-
-for _ in range(stu_num):
-    G, T = map(int, input().split())
-
-    if G == 1:
-        for i in range(T-1, N, T):
-            if switch[i] == 0:
-                switch[i] = 1
-            else:
-                switch[i] = 0
-    else:
-        if switch[T-1] == 0:
-            switch[T-1] = 1
-        else:
-            switch[T-1] = 0
-
-        for i in range(1, N // 2):
-            if T - 1 - i < 0 or T - 1 + i >= N:
-                continue
-            elif switch[T - 1 - i] == switch[T - 1 + i]:
-                if switch[T - 1 - i] == 0:
-                    switch[T - 1 - i] = 1
-                    switch[T - 1 + i] = 1
-                else:
-                    switch[T - 1 - i] = 0
-                    switch[T - 1 + i] = 0
-            else:
-                break
-
-for i in range(N):
-    print(switch[i], end=" ")
-    if i % 20 == 19:
-        print()
+<html>
+<head><title>502 Bad Gateway</title></head>
+<body>
+<center><h1>502 Bad Gateway</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
