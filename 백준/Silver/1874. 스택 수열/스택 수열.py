af433253c14324435f8dac396fd0aeffd540475c
g2vssw <g2vssw@gmail.com>
2026-04-23 20:49:46 +0900

    [Silver II] Title: 스택 수열, Time: 240 ms, Memory: 42060 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+N = int(input())
+
+li = deque()
+for _ in range(N):
+    n = int(sys.stdin.readline())
+    li.append(n)
+
+D = deque()
+for num in range(1, N+1):
+    D.append(num)
+
+pm = []
+R = deque()
+while D:
+    R.append(D.popleft())
+    pm.append("+")
+    while R:
+        if li[0] != R[-1]:
+            break
+        elif li[0] == R[-1]:
+            R.pop()
+            li.popleft()
+            pm.append("-")
+
+if li:
+    print("NO")
+else:
+    for result in pm:
+        print(result)
