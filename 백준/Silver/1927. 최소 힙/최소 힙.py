336cb59d7aaa9d480eb085c8e4cf157eb03432c4
g2vssw <g2vssw@gmail.com>
2026-04-25 19:33:58 +0900

    [Silver II] Title: 최소 힙, Time: 104 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+ 
+N = int(input())
+heap = []
+ 
+for _ in range(N):
+    num = int(input())
+ 
+    if num > 0:
+        heapq.heappush(heap, num)
+    else:
+        if heap:
+            print(heapq.heappop(heap))
+        else:
+            print(0)
