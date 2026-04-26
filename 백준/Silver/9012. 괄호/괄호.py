7c1b085b4594520db45767c1f227966ab463c6a0
g2vssw <g2vssw@gmail.com>
2026-04-27 02:26:19 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+for _ in range(N):
+    line = input()
+    stack = []
+
+    for i in line:
+        if i == '(':
+            stack.append('(')
+        elif i == ')':
+            if len(stack) == 0:
+                stack.append(')')
+                break
+            else:
+                stack.pop()
+
+    if len(stack) != 0:
+        print('NO')
+    else:
+        print('YES')
